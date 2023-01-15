e622fc2703b01ba91c3d163c5500425ca863d996
Mudcrabq <vitalik.berezhnov@gmail.com>
2023-01-15 23:34:18 +0300

    main adaptive

---
+++ web/burger.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function() {
+    $('.border-burger').click(fucntion(event){
+        $('.border-burger,.header-menu').toggleClass('active');
+    });
+});*/
+
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('.burger').classList.toggle('active');
+    document.querySelector('.header-menu').classList.toggle('active');
+    document.querySelector('body').classList.toggle('lock');
+  });
